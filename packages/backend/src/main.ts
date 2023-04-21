e90649d110ffbf2efab104cfedddfb03f1a5c327
mateusz <mateuszmarcinkowski44@gmail.com>
2023-04-21 16:18:55 +0200

    feat(backend/utils): Http exception filter, validation pipe, swagger setup

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './utils/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Shop API swagger')
+    .setDescription('NestJS Shop API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
-bootstrap();
+
+void bootstrap();
