ac328079c97b42b31b85853731509e061f8828f1
mateusz <mateuszmarcinkowski44@gmail.com>
2023-05-06 20:41:21 +0200

    feat(backend): add category routes

--- packages/backend/src/category/category.controller.ts
+++ packages/backend/src/category/category.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dtos/create-category.cto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Post('/add')
+  async createCategory(@Body() body: CreateCategoryDto) {
+    await this.categoryService.createCategory(body.name);
+
+    return {
+      success: true,
+    };
+  }
+
+  @Get('')
+  async getCategories() {
+    return await this.categoryService.getCategories();
+  }
+}
--- packages/backend/src/category/category.service.ts
+++ packages/backend/src/category/category.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../utils/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  createCategory(name: string) {
+    return this.prisma.category.create({
+      data: { name },
+    });
+  }
+
+  getCategories() {
+    return this.prisma.category.findMany();
+  }
+}
