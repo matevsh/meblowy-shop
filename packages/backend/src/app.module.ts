a70505aca8d6fa7fb3c7f92a1463e77088fd6330
mateusz <mateuszmarcinkowski44@gmail.com>
2023-05-07 18:35:37 +0200

    feat(backend): product module initial

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './utils/prisma.service';
 import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, CategoryModule],
+  imports: [AuthModule, CategoryModule, ProductModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
