55889437e3c596e537f0f9063a115f4db20361ad
mateusz <mateuszmarcinkowski44@gmail.com>
2023-05-13 13:54:43 +0200

    chore(backend): setup cors

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,8 @@
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
-
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +14,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
 
   app.use(
     session({
