6c4e162fe628c5a6882cfa73f0f37b601c960326
mateusz <mateuszmarcinkowski44@gmail.com>
2023-05-17 15:07:55 +0200

    fix(backend/prisma): fix typo

--- packages/backend/src/product/product.service.ts
+++ packages/backend/src/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { PrismaService } from '../shared/prisma/prisma.service';
 
+// eslint-disable-next-line prettier/prettier
 @Injectable()
 export class ProductService {
   constructor(private prisma: PrismaService) {}
@@ -44,6 +45,7 @@ export class ProductService {
   }
 
   getProduct(productId: number) {
+
     return this.prisma.item.findFirst({
       where: {
         id: productId
@@ -55,7 +57,7 @@ export class ProductService {
           }
         },
         category: true,
-        Rateing: true
+        Rating: true
       }
     })
   }
